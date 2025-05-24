67424a56c0ea54974453727c6c3bfbd25260fcc0
Michael-Jebaraj <michaeljebaraj29@gmail.com>
2025-05-24 20:05:04 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -94,5 +94,5 @@ app.get('/user/:userId/progress', async (req, res) => {
   }
 });
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
